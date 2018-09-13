dc59cda89213b63fc4673029aaaf32abb9824093
JazminTorres <42577539+JazminTorres@users.noreply.github.com>
2018-09-13 18:04:09 -0500

    Update index.js
    Creación de usuarios

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a CiberQuizz CUCEI!');
 });
 
-app.get('/users', (req, res)=> {
-    const persona = {
-        name: 'Juan',
-        mail: 'Juan@gmail.com',
-    };
-    console.log(persona);
-    res.send(persona);
+app.get('/users', (req, res) => {
+  const user = [
+    {
+      id: 1,
+      name: 'Juan',
+      mail: 'Juan@correo',
+    },
+  ];
+  
+  res.send(user);
+  
 });
+
+app.get('/users/:userId', (req, res) => {
+  const user = {
+    id: req.params.userId,
+    name: 'Juan${req.params.userId}',
+    mail: 'Juan${req.params.userId}@correo',
+  };
+  
+  res.send(user);
+  
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
