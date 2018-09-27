f117b708482247a8c6210780178055e3b670b090
david <david_xd1996@live.com.mx>
2018-09-26 22:04:33 -0500

    se agrego validacion de usuario y contraseña

--- middlewares/users.js
+++ middlewares/users.js
@@ -15,3 +15,30 @@ exports.requiredFields = (req, res, next) => {
         next();
     }
 };
+
+exports.username = (req, res, next) => {
+    var email = /\S+@\S+\.\S+/
+    if(!(email.test(req.body.username))){
+	     res.send('correo electroniuco no valido');
+  } else {
+  next();
+  }
+}
+
+
+exports.password = (req, res, next) => {
+/*
+Minimo 8 caracteres
+Maximo 15
+Al menos una letra mayúscula
+Al menos una letra minucula
+Al menos un dígito
+No espacios en blanco
+*/
+    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    if(regex.test(req.body.password)){
+    res.send('la contrasena no cumple los requisitos')
+  }else {
+    next();
+  }
+}
