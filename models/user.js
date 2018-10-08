7d10d8141529ed0dab730ed376e09d040b3a3f32
Castolo10 <kaztolo10@gmail.com>
2018-10-07 23:51:17 -0500

    Agregados modelos referentes a usuarios y sus amigos

--- models/user.js
+++ models/user.js
@@ -10,7 +10,9 @@ exports.create = function(username, name, mail, password, roles = false, exist =
 };
 
 exports.find = (username) => {
-  var response = db.SELECT('user', '*', `username=\'${username}\'`);
+  var response = db.SELECT('user',
+  '*',
+  `username=\'${username}\'`);
   return response;
 };
 
@@ -20,19 +22,56 @@ exports.list = () => {
 };
 
 exports.auth = (username,password) => {
-  var response = db.SELECT('user', '*', `username=\'${username}\' AND password=\'${password}\'`);
+  var response = db.SELECT('user',
+  '*',
+  `username=\'${username}\' AND password=\'${password}\'`);
   return response;
 };
 
 exports.edit = (username, name, mail, password, roles) => {
   var response = db.UPDATE('user',
-  `name=\'${name}\', mail=\'${email}\', password=\'${password}\', roles=\'${roles}\'`,
+  `name=\'${name}\', mail=\'${email}\', password=\'${password}\',
+  roles=\'${roles}\'`,
   `username=${username}`,
   `*`);
   return response;
 };
 
 exports.delete = (username) => {
-  var response = db.UPDATE(user, 'exist=false',`username=${username}`);
+  var response = db.UPDATE(user, 'exist=false',
+  `username=${username}`);
   return response;
 };
+
+exports.addFriend = (username, friend) => {
+  var response = db.INSERT('friendlist',
+  'username, friend_username, status_req',
+  `username=\'${username}\','friend_username=\'${friend}\', false`);
+}
+
+exports.findFriend = (username) => {
+  var response = db.SELECT('friendlist',
+  'friend_username',
+  `friend_username=\'${username}\' AND status_req=true`);
+  return response;
+};
+
+exports.friendList = (username) => {
+  var response = db.SELECT('friendlist',
+  'friend_username',
+  `username=\'${username}\'`);
+  return response;
+}
+
+exports.editStatusFriend = (username, friend_username) => {
+  var response = db.UPDATE('friendlist',
+  'status_req=false',
+  `username=\'${username}\'AND friend_username=\'${friend}\'`);
+  return response;
+}
+
+exports.deleteFriend = (username, friend_username) => {
+  var response = db.DELETE('friendlist',
+  `username=\'${username}\'AND friend_username=\'${friend}\'`);
+  return response;
+}
